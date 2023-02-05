583e12ed07413d2aae9c27a307c9be02d7586042
Edgar200122 <edgar.dzavaryan2001@mail.ru>
2023-02-05 23:38:45 +0400

    NewsApp
    NewsApp

---
+++ app/src/main/java/ru/mvlikhachev/newsapp/utils/Constants.kt
@@ -0,0 +1,8 @@
+package ru.mvlikhachev.newsapp.utils
+
+class Constants {
+    companion object {
+        const val BASE_URL = "https://newsapi.org/"
+        const val API_KEY = "16f17b3107794837b22a1d340d35e528"
+    }
+}
